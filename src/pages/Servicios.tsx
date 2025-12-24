668d6a61ce512196dae79cf333b6eaa14995c3dd
mapm90 <mpmorales05@gmail.com>
2025-12-24 20:46:19 +0100

    Primer commit frontend

--- src/pages/Servicios.tsx
+++ src/pages/Servicios.tsx
@@ -270,7 +270,7 @@ const Servicios = () => {
                   ref={ref}
                   initial={{ opacity: 0, y: 1 }}
                   animate={inView ? { opacity: 1, y: 0 } : {}}
-                  transition={{ duration: 0.1 }}
+                  transition={{ duration: 0.5 }}
                 >
                   <ServiceCard {...service} index={index} />
                 </motion.div>
