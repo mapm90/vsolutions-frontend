76f0e205752d1874392be17f2e778797647e9dd6
miguel <tuemail@example.com>
2026-01-01 19:01:46 +0100

    Normalize line endings

--- src/pages/api/fetchapi.ts
+++ src/pages/api/fetchapi.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://vsolutions-backendd.vercel.app/api";
+const BASE_URL = "https://vsolutions-backendd.vercel.app/api";
 
 export async function apiFetch<T>(
   endpoint: string,
