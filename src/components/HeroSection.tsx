5b7eedc81474e4095e5f723e762d707019eb9bc9
miguel <tuemail@example.com>
2026-01-07 20:52:38 +0100

    Normalize line endings

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -71,7 +71,7 @@ const HeroSection = () => {
                 </h1>
                 <div className="text-lg text-muted-foreground leading-relaxed text-justify">
                   <p className="font-semibold mb-2">
-                    Soy Verónica Borges, fundadora de{" "}
+                    Soy sergio gonzalez, fundadora de{" "}
                     <span className="whitespace-nowrap">V-Services</span>
                   </p>
                   <p>
